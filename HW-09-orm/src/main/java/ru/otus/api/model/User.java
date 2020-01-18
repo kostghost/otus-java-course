3f662cdaf9478945d9a76a0eec7f3ce4a9ea09bc
Konstantin Loginov <kostghost@gmail.com>
2020-01-18 21:13:48 +0500

    HW09: delete all unnecessary code

--- HW-09-orm/src/main/java/ru/otus/api/model/User.java
+++ HW-09-orm/src/main/java/ru/otus/api/model/User.java
@@ -1,16 +1,22 @@
 package ru.otus.api.model;
 
+import ru.otus.api.annotations.Id;
+
 /**
  * @author sergey
  * created on 03.02.19.
  */
-public class User {
+public class User implements HasIdModel {
+
+    @Id
     private final long id;
     private final String name;
+    private final int age;
 
-    public User(long id, String name) {
+    public User(long id, String name, int age) {
         this.id = id;
         this.name = name;
+        this.age = age;
     }
 
     public long getId() {
@@ -21,11 +27,16 @@ public class User {
         return name;
     }
 
+    public int getAge() {
+        return age;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", age=" + age +
                 '}';
     }
 }
--- HW-09-orm/src/main/java/ru/otus/jdbc/ExecutorDemo.java
+++ HW-09-orm/src/main/java/ru/otus/jdbc/ExecutorDemo.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -16,6 +16,18 @@ import ru.otus.api.model.User;
  * created on 03.02.19.
  */
 public class ExecutorDemo {
+    private static final String CREATE_USER_TABLE_SQL = "" +
+            "create table user(" +
+            "   id bigint(20) not null auto_increment, " +
+            "   name varchar(255), " +
+            "   age int(3))";
+
+    private static final String CREATE_ACCOUNT_TABLE_SQL = "" +
+            "create table account(" +
+            "   no bigint(20) not null auto_increment, " +
+            "   type varchar(255), " +
+            "   rest number)";
+
     private static final String URL = "jdbc:h2:mem:";
     private static Logger logger = LoggerFactory.getLogger(ExecutorDemo.class);
 
@@ -24,32 +36,37 @@ public class ExecutorDemo {
 
         try (Connection connection = DriverManager.getConnection(URL)) {
             connection.setAutoCommit(false);
-            demo.createTable(connection);
+            demo.createTable(connection, CREATE_USER_TABLE_SQL);
+            demo.createTable(connection, CREATE_ACCOUNT_TABLE_SQL);
+
 
             DbExecutor<User> executor = new DbExecutor<>();
-            long userId = executor.insertRecord(connection, "insert into user(name) values (?)",
-                    Collections.singletonList("testUserName"));
+            long userId = executor.insertRecord(connection, "insert into user(name, age) values (?, ?)",
+                    List.of("testUserName", "21"));
             logger.info("created user:{}", userId);
             connection.commit();
 
-            Optional<User> user = executor.selectRecord(connection, "select id, name from user where id  = ?", userId
+            Optional<User> user = executor.selectRecord(connection, "select id, name, age from user where id  = ?",
+                    userId
                     , resultSet -> {
-                try {
-                    if (resultSet.next()) {
-                        return new User(resultSet.getLong("id"), resultSet.getString("name"));
-                    }
-                } catch (SQLException e) {
-                    logger.error(e.getMessage(), e);
-                }
-                return null;
-            });
+                        try {
+                            if (resultSet.next()) {
+                                return new User(
+                                        resultSet.getLong("id"),
+                                        resultSet.getString("name"),
+                                        resultSet.getInt("age"));
+                            }
+                        } catch (SQLException e) {
+                            logger.error(e.getMessage(), e);
+                        }
+                        return null;
+                    });
             System.out.println(user);
         }
     }
 
-
-    private void createTable(Connection connection) throws SQLException {
-        try (PreparedStatement pst = connection.prepareStatement("create table user(id long auto_increment, name varchar(50))")) {
+    private void createTable(Connection connection, String sql) throws SQLException {
+        try (PreparedStatement pst = connection.prepareStatement(sql)) {
             pst.executeUpdate();
         }
     }
